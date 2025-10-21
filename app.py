7fea718867bf2a12c5e8f0fdec0ccb4026e409ec
danebishop <bishop_dane@hotmail.com>
2025-10-21 18:28:18 +0000

    Added link to actual players

--- app.py
+++ app.py
@@ -117,12 +117,31 @@ def team_detail(team_slug: str):
 #players page
 @app.route("/players")
 def players():
+
+
+
     sample_players = [
         {"name": "Alex Johnson", "team": "Football", "position": "QB"},
         {"name": "Sam Lee", "team": "Men's Basketball", "position": "G"},
         {"name": "Taylor Smith", "team": "Women's Soccer", "position": "FW"},
     ]
-    return render_template("players.html", players=sample_players)
+
+    sql = """
+    SELECT DISTINCT
+    p.id, p.full_name, s.name, rm.position
+    FROM players p
+    JOIN roster_memberships rm ON rm.player_id = p.id
+    JOIN team_seasons ts ON rm.team_season_id = ts.id
+    JOIN teams t ON ts.team_id = t.id
+    JOIN sports s ON t.sport_id = s.id
+    ORDER BY name;
+    """
+
+    with conn.cursor(cursor_factory=RealDictCursor) as cur:
+        cur.execute(sql)
+        players = cur.fetchall()
+
+    return render_template("players.html", players=players)
 
 
 
